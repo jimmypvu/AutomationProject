bc339c6a47ee1f23b8fe211791daa5074f2e0f5f
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-02-20 23:06:34 -0800

    login invalid credentials (dataprovider)

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,17 +10,6 @@ import pages.LoginPage;
 public class LoginTests extends BaseTest{
     @Test(enabled = true, priority = 0, description = "login with valid email / valid password")
     public static void loginValidEmailValidPassword(){
-//        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#username")));
-//        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#password")));
-//        WebElement stayLoggedInBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='form-check-input']")));
-//        WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#log-in")));
-//
-//        action.click(emailField).sendKeys("testemail@gmail.com").perform();
-//        action.click(passwordField).sendKeys("password").perform();
-//        stayLoggedInBox.click();
-//        loginButton.click();
-//
-//        Assert.assertTrue(wait.until(ExpectedConditions.urlMatches("https://demo.applitools.com/app.html")));
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("testemail@email.com", "password");
         loginPage.isUserLoggedIn();
